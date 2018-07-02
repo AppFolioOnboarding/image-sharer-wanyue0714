f0a3a0706d15514ffe0baddfcc9c347d00d3e618
boying.shi <shi.boying@gmail.com>
2018-07-01 18:00:01 -0700

    bs - install packages for test setup

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'application#home'
 
+  resources :images
+
   resources :feedbacks, only: [:new]
 
   namespace :api do
