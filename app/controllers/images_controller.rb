b2a873940d0445f4aae2ac5e9a3421a0e5835e2e
Yue Wan <yue.wan@appfolio.com>
2019-02-20 14:51:51 -0800

    yw - add new and create to images controller

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -2,4 +2,23 @@ class ImagesController < ApplicationController
   def index
     @images = Image.all
   end
+
+  def new
+    @image = Image.new
+  end
+
+  def create
+    @image = Image.new(image_params)
+    if @image.save
+      redirect_to image_path(@image)
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def image_params
+    params.require(:image).permit(:title, :link)
+  end
 end
--- test/controllers/images_controller_test.rb
+++ test/controllers/images_controller_test.rb
@@ -5,4 +5,20 @@ class ImagesControllerTest < ActionDispatch::IntegrationTest
     get images_path
     assert_response :success
   end
+
+  def test_shoule_create_new_image
+    image = Image.create!(title: 'piggy',
+                          link: 'https://s.abcnews.com/images/Lifestyle/AP_micro_pigs_1_sr_140319_mn_16x9_992.jpg')
+
+    assert_difference('Image.count') do
+      post images_path, params: { image: { title: image.title, link: image.link } }
+    end
+  end
+
+  def test_should_get_new_image_path
+    get new_image_path
+
+    assert_response :success
+    assert_includes body, 'Back to Homepage'
+  end
 end
