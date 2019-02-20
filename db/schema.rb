7534e99d3e57f714d2578145c3821ba085634ac0
Yue Wan <yue.wan@appfolio.com>
2019-02-20 14:23:36 -0800

    yw - create image table in db

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
-
+ActiveRecord::Schema.define(version: 2019_02_20_074259) do
+  create_table "images", force: :cascade do |t|
+    t.string "title"
+    t.string "link"
+  end
 end
