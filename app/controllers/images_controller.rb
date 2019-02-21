95b65c78a40b0462b0627f2b499cae00a8a8f1f9
Yue Wan <yue.wan@appfolio.com>
2019-02-20 17:36:55 -0800

    yw - add update and edit to image controllers

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -20,6 +20,19 @@ class ImagesController < ApplicationController
     @image = Image.find(params[:id])
   end
 
+  def edit
+    @image = Image.find(params[:id])
+  end
+
+  def update
+    @image = Image.find(params[:id])
+    if @image.update(image_params)
+      redirect_to image_path(@image)
+    else
+      render 'edit'
+    end
+  end
+
   private
 
   def image_params
--- test/controllers/images_controller_test.rb
+++ test/controllers/images_controller_test.rb
@@ -28,4 +28,26 @@ class ImagesControllerTest < ActionDispatch::IntegrationTest
     get image_path(image)
     assert_response :success
   end
+
+  def test_should_update_image
+    image = Image.create!(title: 'piggy',
+                      link: 'https://s.abcnews.com/images/Lifestyle/AP_micro_pigs_1_sr_140319_mn_16x9_992.jpg')
+    patch image_path(image), params: { image: { title: 'plant',
+                                               link: 'https://s.abcnews.com/images/Lifestyle/2.jpg' } },
+                            xhr: true
+    assert_redirected_to image_path(image)
+
+    image.reload
+    assert_equal 'https://s.abcnews.com/images/Lifestyle/2.jpg', image.link
+  end
+
+  def test_should_edit
+    image = Image.create!(title: 'piggy',
+                          link: 'https://s.abcnews.com/images/Lifestyle/AP_micro_pigs_1_sr_140319_mn_16x9_992.jpg')
+
+    get edit_image_path(image)
+
+    assert_response :success
+    assert_includes body, 'Back to Homepage'
+  end
 end
