4a36bd5b0f008374b209ef0bc32e061e557ec359
Yue Wan <yue.wan@appfolio.com>
2019-03-03 02:46:50 -0800

    yw - fix the url validation bug in model

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,5 +1,5 @@
 class Image < ApplicationRecord
-  URL_REGEX = %r{(https?:\/\/.*\.(?:png|jpg|gif|tif))}i
+  URL_REGEX = %r{(https?:\/\/.*\.(?:png|jpg|gif|tif))}i.freeze
   validates :title, presence: true, length: { minimum: 3, maximum: 50 }
   validates :link, presence: true, format: { with: URL_REGEX, message: 'is invalid URL for image' }
   acts_as_taggable
