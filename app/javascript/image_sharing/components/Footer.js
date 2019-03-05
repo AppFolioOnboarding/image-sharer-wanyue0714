4b814384c82df20c6034edb1368f0a0f4de9d8b9
Yue Wan <yue.wan@appfolio.com>
2019-03-05 11:05:52 -0800

    yw - create copyright in components/Footer.js

--- app/javascript/image_sharing/components/Footer.js
+++ app/javascript/image_sharing/components/Footer.js
@@ -1,7 +1,20 @@
 import React, { Component } from 'react';
+import { Row, Col } from 'reactstrap';
+import PropTypes from 'prop-types';
 
 class Footer extends Component {
-  /* Implement your Footer component here */
+  static propTypes = {
+    copyright: PropTypes.string.isRequired
+  };
+
+  render() {
+    const copyright = this.props.copyright;
+    return (
+      <div className = 'Footer'>
+        {copyright}
+      </div>
+    )
+  }
 }
 
 export default Footer;
